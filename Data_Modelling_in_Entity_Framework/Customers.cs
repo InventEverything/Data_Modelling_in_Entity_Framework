9586cf5462bc8f44c27622d206dddf1efdf779e5
ian engle <ian.engle@student.centralia.edu>
2024-11-06 13:06:28 -0800

    Added relationship to customer and nullable requirements as notes.

--- Data_Modelling_in_Entity_Framework/Customers.cs
+++ Data_Modelling_in_Entity_Framework/Customers.cs
@@ -8,15 +8,15 @@ namespace Data_Modelling_in_Entity_Framework
 {
     internal class Customers
     {
-        public int CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public DateOnly Birthday { get; set; }
-        public string PhoneNumbers { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
-        public string Likes { get; set; }
+        public int CustomerId { get; set; }     // single unique,                           not nullable
+        public string FirstName { get; set; }   // single,                                  not nullable
+        public string MiddleName { get; set; }  // single,                                      nullable
+        public string LastName { get; set; }    // single,                                  not nullable
+        public DateOnly Birthday { get; set; }  // single,                                  not nullable
 
+        public string PhoneNumbers { get; set; } // one customer can have many phone numbers,   nullable
+        public string Address { get; set; }     // many customers can live at one address,      nullable
+        public string Email { get; set; }       // single,                                      nullable
+        public string Likes { get; set; }       // many customers can have many likes,          nullable
     }
 }
