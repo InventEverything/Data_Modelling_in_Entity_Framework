d8fe996a44cba4fa6042433a68588a9583f06431
ian engle <ian.engle@student.centralia.edu>
2024-11-06 12:50:25 -0800

    Added base data as required by assignment. Next step is normalization.

---
+++ Data_Modelling_in_Entity_Framework/Customers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Modelling_in_Entity_Framework
+{
+    internal class Customers
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly Birthday { get; set; }
+        public string PhoneNumbers { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Likes { get; set; }
+
+    }
+}
